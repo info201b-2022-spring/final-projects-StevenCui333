1ed8802c04cd6a4a10437f4f3afb0390788dc0cb
StevenCui333 <tcui2@uw.edu>
2022-05-20 18:12:25 -0700

    dkljfas

--- analyzation/Table_Variables.R
+++ analyzation/Table_Variables.R
@@ -4,20 +4,27 @@ library(tidyverse)
 game <- read.csv("video.csv")
 summary_info <- list()
 
-
+ 
+#the total amount of game sales in Japan
 JapanSaleTotal <- sum(game$JP_Sales)
 
+#the total amount of game sale in North America
 NASaleTotal <-  sum(game$NA_Sales)
 
+#the total amount of game sales globally
 globalSaleTotal <- sum(na.omit(game$Global_Sales))
-                   
+
+#the amount the of the games that Nietendo has published                
 Amount_of_Nietendo_Published_games <- sum((game$Publisher == "Nintendo"))
 
+#and total published games
 published_games_in_total <- nrow(game)
+ 
   
-Most_publish_Genre_of_Game <- game %>% group_by(game$Year_of_Release, game$Genre) %>% na.omit(summarise((max(sum(game$JP_Sales))), count = count(game$Name)))
-  
-table <- game %>% group_by(Genre) %>% filter(Global_Sales == min(Global_Sales)) 
+table <- game %>% group_by(Genre) %>% summarize(Name == Name[max_sales = max(Global_Sales)], 
+                                                max_sales = max(Global_Sales), 
+                                                User_Rate = max(User_Rate), 
+                                                Publisher == Publisher[max_sales = max(Global_Sales)])
 
 
 
