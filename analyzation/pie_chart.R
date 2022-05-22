7421297c676c90bb96a1329576772f1781c5870d
FXF48-Mila <liuyis@uw.edu>
2022-05-22 14:17:39 -0700

    final version

--- analyzation/pie_chart.R
+++ analyzation/pie_chart.R
@@ -6,4 +6,10 @@ sports_by_area <- games %>%
   filter(Genre == 'Sports')
 Sales <- apply(sports_by_area[,c(6,7,8,9)], 2, sum)
 data <- data.frame(Sales, area = c('North America', 'Europe', 'Japan', 'Rest of World'))
-chart_1 <- ggplot(data, aes(x= "Sports", y = Sales, fill = area)) + geom_bar(stat = 'identity') + coord_polar(theta = "y")
+chart_1 <- ggplot(data, aes(x= "Sports", y = Sales, fill = area)) + 
+  geom_bar(stat = 'identity') + 
+  coord_polar(theta = "y") +
+  labs(title = "Sales Percentage of Sports Genres of Each Region", 
+       fill = "Region",
+       x = ""
+       )
--- analyzation/sales_by_year_release.r
+++ analyzation/sales_by_year_release.r
@@ -19,5 +19,7 @@ plot <- Sports_df%>%
         ggplot( aes(x=Year_of_Release, y=Sports_sales)) +
         geom_line( color="grey") +
         geom_point(shape=21, color="black", fill="#69b3a2", size=6) +
-        ggtitle("Global Sports Games Sales by different year of release ") + labs(y = "Sports_sales(in millions)")
+        ggtitle("Global Sports Games Sales by Different Year of Release ") + 
+  labs(y = "Sports Sales (in millions)", 
+       x = "Year of Release")
 
