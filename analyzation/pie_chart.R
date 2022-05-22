cb2a4fb3ce4e3a659d1a45a4e65e0b4e5b01f334
郜云笛 <ygao27@uw.edu>
2022-05-22 10:13:11 -0700

    pie

--- analyzation/pie_chart.R
+++ analyzation/pie_chart.R
@@ -1,9 +1,9 @@
 library(tidyverse)
 library(ggplot2)
 games <- read.csv("Video_Games_Sales_as_at_22_Dec_2016.csv")
+View(games)
 sports_by_area <- games %>%
   filter(Genre == 'Sports')
-Sales = apply(sports_by_area[,c(5,6,7,8)], 2, sum)
+Sales <- apply(sports_by_area[,c(6,7,8,9)], 2, sum)
 data <- data.frame(Sales, area = c('North America', 'Europe', 'Japan', 'Rest of World'))
-
-chart_1 <- ggplot(data, aes(x = "Sports", y = Sales, fill = area)) + geom_bar(stat = 'identity') + coord_polar(theta = "y")
+ggplot(data, aes(x= "Sports", y = Sales, fill = area)) + geom_bar(stat = 'identity') + coord_polar(theta = "y")
