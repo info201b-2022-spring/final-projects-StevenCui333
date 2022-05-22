ca853175aae1249cf485d40487a54b685a38d342
FXF48-Mila <liuyis@uw.edu>
2022-05-22 10:40:23 -0700

    summary

---
+++ analyzation/summary info.R
@@ -0,0 +1,14 @@
+#the total amount of game sales in Japan
+JapanSaleTotal <- sum(game$JP_Sales)
+
+#the total amount of game sale in North America
+NASaleTotal <-  sum(game$NA_Sales)
+
+#the total amount of game sales globally
+globalSaleTotal <- sum(na.omit(game$Global_Sales))
+
+#the amount the of the games that Nietendo has published                
+Amount_of_Nietendo_Published_games <- sum((game$Publisher == "Nintendo"))
+
+#and total published games
+published_games_in_total <- nrow(game)
