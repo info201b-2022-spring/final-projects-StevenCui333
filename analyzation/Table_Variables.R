b834929803a3a8f3ab55dfe9fd40f0666942a6c3
StevenCui333 <tcui2@uw.edu>
2022-05-22 13:53:52 -0700

    dfasf

--- analyzation/Table_Variables.R
+++ analyzation/Table_Variables.R
@@ -4,22 +4,6 @@ library(tidyverse)
 game <- read.csv("Video_Games_Sales_as_at_22_Dec_2016.csv")
 
 
-#the total amount of game sales in Japan
-JapanSaleTotal <- sum(game$JP_Sales)
-
-#the total amount of game sale in North America
-NASaleTotal <-  sum(game$NA_Sales)
-
-#the total amount of game sales globally
-globalSaleTotal <- sum(na.omit(game$Global_Sales))
-
-#the amount the of the games that Nietendo has published                
-Amount_of_Nietendo_Published_games <- sum((game$Publisher == "Nintendo"))
-
-#and total published games
-published_games_in_total <- nrow(game)
-
-
 #The table is grouped by the Genre of the games, 
 #and find out which game in their genre have the highest sale record, who are their publishers. 
 #We also include the user rate to see if sales will affect the user rate.
--- analyzation/summary info.R
+++ analyzation/summary info.R
@@ -8,3 +8,18 @@ summary_info$globalSaleTotal <- sum(na.omit(game$Global_Sales))
 summary_info$NASaleTotal <-  sum(game$NA_Sales)
 summary_info$JapanSaleTotal <- sum(game$JP_Sales)
 summary_info$Amount_of_Nietendo_Published_games <- sum((game$Publisher == "Nintendo"))
+
+#the total amount of game sales in Japan
+JapanSaleTotal <- sum(game$JP_Sales)
+
+#the total amount of game sale in North America
+NASaleTotal <-  sum(game$NA_Sales)
+
+#the total amount of game sales globally
+globalSaleTotal <- sum(na.omit(game$Global_Sales))
+
+#the amount the of the games that Nietendo has published                
+Amount_of_Nietendo_Published_games <- sum((game$Publisher == "Nintendo"))
+
+#and total published games
+published_games_in_total <- nrow(game)
